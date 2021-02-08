a8a413bc49b93fd4cd9150975c89943e97d8445d
WeihangMeng <mwh1312119219@gmail.com>
2021-02-08 03:46:28 -0500

    Update detailPage.js

--- src/components/detailPage.js
+++ src/components/detailPage.js
@@ -4,31 +4,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import Loader from "./loader";
 
-// adult: false
-// backdrop_path: "/lOSdUkGQmbAl5JQ3QoHqBZUbZhC.jpg"
-// belongs_to_collection: null
-// budget: 0
-// genres: Array(3)
-// homepage: "https://www.netflix.com/title/81074110"
-// id: 775996
-// imdb_id: "tt10451914"
-// original_language: "en"
-// original_title: "Outside the Wire"
-// overview: "In the near future, a drone pilot is sent into a deadly militarized zone and must work with an android officer to locate a doomsday device."
-// popularity: 3519.784
-// poster_path: "/e6SK2CAbO3ENy52UTzP3lv32peC.jpg"
-// production_companies: Array(4)
-// production_countries: Array(1)
-// release_date: "2021-01-15"
-// revenue: 0
-// runtime: 114
-// spoken_languages: Array(1)
-// status: "Released"
-// tagline: "Defiant by design."
-// title: "Outside the Wire"
-// video: false
-// vote_average: 6.5
-// vote_count: 474
+
 const Container = styled.div`
   width: 800px;
   margin: auto;
@@ -46,13 +22,10 @@ class DetailPage extends React.Component {
     rate: 0,
     isLogging: false,
     ratedList: [],
-    currentRate: 0
+    currentRate: 0,
+    genres: [],
   };
-  // componentDidUpdate(prev) {
-  //   if (prev.ratedList !== this.state.ratedList) {
-  //     this.setState({ rateUpdate: true });
-  //   }
-  // }
+  
   componentDidMount() {
     this.setState(
       {
@@ -69,22 +42,18 @@ class DetailPage extends React.Component {
         this.setState({
           rate: this.state.isRated
             ? this.props.ratedList.find((movie) => movie.id === this.state.id)
-                .rate
+                .rating
             : 0
         });
       }
     );
 
     fetch(
-      // `https://api.themoviedb.org/3/movie/560144?api_key=31846cd2c427dd933fa6849953b3974d`
       `https://api.themoviedb.org/3/movie/${this.props.currentDetail}?api_key=31846cd2c427dd933fa6849953b3974d`
     )
       .then((resp) => resp.json())
       .then((resp) => {
-        this.setState({ detail: resp }, () => {
-          console.log(this.state.detail);
-          console.log(this.state.id);
-        });
+        this.setState({ detail: resp });
       });
     const { match, location, history } = this.props;
     this.props.getAddress(this.props.location.pathname);
@@ -99,15 +68,6 @@ class DetailPage extends React.Component {
       <Container>
         <div className="detail-page">
           <div className="rate-page">
-            {/* <div
-              className="rate-success"
-              style={{ display: this.props.rateSuccess ? "" : "none" }}
-            >
-              <div className="rate-container">
-                <span>Rate Success!</span>
-              </div> */}{" "}
-            */}
-            {/* </div> */}
             <div className="Poster">
               <img
                 className="poster"
@@ -121,21 +81,15 @@ class DetailPage extends React.Component {
               <span>{this.state.detail.release_date}</span>
               <h2>Overview</h2>
               <p>{this.state.detail.overview}</p>
-              <h2
-                onClick={(e) => {
-                  console.log(this.state.detail.genres);
-                }}
-              >
-                Genres
-              </h2>
-              {/* {this.state.detail.genres} */}
+             {/* <h2>Genres</h2> */}
+              {/* {this.state.genres.map((genre) => {
+                return <div className="genre-item">genre.name</div>
+              })} */}
+              
               <h2> Average Rating</h2>
               <span>{this.state.detail.vote_average}</span>
               <h2
-                onClick={(e) => {
-                  console.log("app", this.props.isLogging);
-                  console.log("local", this.state.isLogging);
-                }}
+                
               >
                 {" "}
                 Your Rating!
